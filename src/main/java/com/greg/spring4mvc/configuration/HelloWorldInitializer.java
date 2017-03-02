07a64f6fabfb7138b1b2401fa6904dd6bb352f5c
greg <greg_echo@hotmail.com>
2017-03-02 16:21:29 +0800

    Demo application based on annotation

---
+++ src/main/java/com/greg/spring4mvc/configuration/HelloWorldInitializer.java
@@ -0,0 +1,45 @@
+package com.greg.spring4mvc.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+/*
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class HelloWorldInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(HelloWorldConfiguration.class);
+		ctx.setServletContext(servletContext);
+
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+}*/
+// the preferred way
+public class HelloWorldInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { HelloWorldConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
--- src/main/java/com/greg/spring4mvc/controller/HelloWorldController.java
+++ src/main/java/com/greg/spring4mvc/controller/HelloWorldController.java
@@ -1,25 +1,23 @@
 package com.greg.spring4mvc.controller;
 
-
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class HelloWorldController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String sayHello(ModelMap model){
-		model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+		model.addAttribute("greeting", "Hello World from Spring 4 MVC annotation");
         return "welcome";
 	}
 	
 	@RequestMapping(value="/helloagain", method = RequestMethod.GET)
     public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC annotation");
         return "welcome";
     }
 }
