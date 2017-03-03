408e5170fba49e15a6a69033a29a6740069ec745
greg <greg_echo@hotmail.com>
2017-03-03 09:46:14 +0800

    1. Validatation 2. add bower

--- src/main/java/com/greg/spring4mvc/configuration/HelloWorldConfiguration.java
+++ src/main/java/com/greg/spring4mvc/configuration/HelloWorldConfiguration.java
@@ -1,17 +1,21 @@
 package com.greg.spring4mvc.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan({"com.greg.spring4mvc"})
-public class HelloWorldConfiguration {
+@ComponentScan({ "com.greg.spring4mvc" })
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -21,5 +25,27 @@ public class HelloWorldConfiguration {
 		viewResolver.setSuffix(".jsp");
 
 		return viewResolver;
+	}
+
+	/*
+	 * Configure ResourceHandlers to serve static resources like CSS/ Javascript
+	 * etc...
+	 *
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	/*
+	 * Configure MessageSource to provide internationalized messages
+	 *
+	 */
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
